f1dcc7303ccdc7d6e15e3bea954aa43079d48aac
harsha711 <chinna.puvvadi@gmail.com>
2021-09-26 16:57:38 +0530

    active page

---
+++ build/precache-manifest.995c22547d978c4b356d6c7fb5d9ecba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a009b9f069ffe266ad3a",
+    "url": "/dgram/static/css/main.a11481bb.chunk.css"
+  },
+  {
+    "revision": "a009b9f069ffe266ad3a",
+    "url": "/dgram/static/js/main.a009b9f0.chunk.js"
+  },
+  {
+    "revision": "b1e66cefe83581fe1710",
+    "url": "/dgram/static/css/1.4d442051.chunk.css"
+  },
+  {
+    "revision": "b1e66cefe83581fe1710",
+    "url": "/dgram/static/js/1.b1e66cef.chunk.js"
+  },
+  {
+    "revision": "c3b3baea0e325c60083e",
+    "url": "/dgram/static/js/runtime~main.c3b3baea.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "/dgram/static/media/photo.d6cbd8e6.png"
+  },
+  {
+    "revision": "f6917d57c8efe4ac5d0fb75ba91fa91e",
+    "url": "/dgram/index.html"
+  }
+];
